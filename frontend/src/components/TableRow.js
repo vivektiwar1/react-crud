469672afdb9116a486127fd3d2a47ea1c28c146f
Vivek tiwari <nktiwarippp@gmail.com>
2019-06-18 02:13:01 +0530

    final commit

---
+++ frontend/src/components/TableRow.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+class TableRow extends Component {
+
+  constructor(props) {
+        super(props);
+        this.delete = this.delete.bind(this);
+    }
+
+    componentDidUpdate() {
+        axios.get('http://localhost:4000/business')
+        .then(response => {
+        this.setState({ business: response.data });
+        })
+        .catch(function (error) {
+        console.log(error);
+        })
+        }
+
+    delete() {
+        axios.get('http://localhost:4000/business/delete/'+this.props.obj._id)
+            .then(console.log('Deleted'))
+            .catch(err => console.log(err))
+    }
+  render() {
+    return (
+        <tr>
+          <td>
+            {this.props.obj.person_name}
+          </td>
+          <td>
+            {this.props.obj.business_name}
+          </td>
+          <td>
+            {this.props.obj.business_gst_number}
+          </td>
+          <td>
+            <Link to={"/edit/"+this.props.obj._id} className="btn btn-primary">Edit</Link>
+          </td>
+          <td>
+            <button onClick={this.delete} className="btn btn-danger">Delete</button>
+          </td>
+        </tr>
+    );
+  }
+}
+
+export default TableRow;
